3f71b231393f0489202418b626f1017039180d90
feministAF <santiagoreha24@gmail.com>
2020-03-02 16:45:48 +0530

    first commit

---
+++ linux_commands/list_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO: CRIO_TASK_MODULE_LINUX_COMMANDS
+    # Tasks:
+    # 1) Add commands to list files in the QBox/logs directory in long format, 
+    #    sorted by modification time in reverse order (newest last). 
+    #    This command should work irrespective of the current working directory.
+    # 2) Execute the script before submission and confirm the output
+
+ls -lrt ~/workspace/QBox/logs #l is for long format and t if for modification time
